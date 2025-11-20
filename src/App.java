2d128ad4835d6cdf9adc5b2826908428559df21f
Fabio-Tecno <fmbinho8@gmail.com>
2025-11-20 14:20:10 -0300

    Classes mentoria e curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Programação Java");
+        curso.setDescricao("Curso para exercitar o POO com a linguagem Java");
+        curso.setCargaHoraria(8);
+        
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Lógica de Programação");
+        curso1.setDescricao("Curso para exercitar o raciocínio com a linguagem JavaScript");
+        curso1.setCargaHoraria(15);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Mentoria para abordar detalhes da trilha do curso");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso1);
+        System.out.println(mentoria);
+    
     }
 }
